068e77b40187c917d648540a313272c3b41ad5a6
drathveloper <alberto.hdzhdz@gmail.com>
2025-06-10 20:42:19 +0200

    enabled and configured tagalign linter

--- pkg/config/model.go
+++ pkg/config/model.go
@@ -3,27 +3,27 @@ package config
 import "time"
 
 type Config struct {
-	Gateway Gateway `validate:"required" json:"gateway" yaml:"gateway"`
+	Gateway Gateway `json:"gateway" yaml:"gateway" validate:"required"`
 }
 
 type Gateway struct {
 	HTTPClient    *HTTPClient         `json:"httpclient"     yaml:"httpclient"`
-	Routes        []Route             `validate:"required,min=1,dive" json:"routes" yaml:"routes"`
-	GlobalFilters []ParameterizedItem `validate:"dive"                json:"global-filters" yaml:"global-filters"`
+	Routes        []Route             `json:"routes"         yaml:"routes"         validate:"required,min=1,dive"`
+	GlobalFilters []ParameterizedItem `json:"global-filters" yaml:"global-filters" validate:"dive"`
 	GlobalTimeout Duration            `json:"global-timeout" yaml:"global-timeout"`
 }
 
 type Route struct {
-	ID         string              `validate:"required" json:"id"  yaml:"id"`
-	URI        string              `validate:"required" json:"uri" yaml:"uri"`
-	Predicates []ParameterizedItem `validate:"dive"     json:"predicates" yaml:"predicates"`
-	Filters    []ParameterizedItem `validate:"dive"     json:"filters"    yaml:"filters"`
-	Timeout    Duration            `json:"timeout" yaml:"timeout"`
+	ID         string              `json:"id"         yaml:"id"         validate:"required"`
+	URI        string              `json:"uri"        yaml:"uri"        validate:"required"`
+	Predicates []ParameterizedItem `json:"predicates" yaml:"predicates" validate:"dive"`
+	Filters    []ParameterizedItem `json:"filters"    yaml:"filters"    validate:"dive"`
+	Timeout    Duration            `json:"timeout"    yaml:"timeout"`
 }
 
 type ParameterizedItem struct {
 	Args map[string]any `json:"args" yaml:"args"`
-	Name string         `validate:"required" json:"name" yaml:"name"`
+	Name string         `json:"name" yaml:"name" validate:"required"`
 }
 
 type HTTPClient struct {
@@ -34,20 +34,20 @@ type HTTPClient struct {
 }
 
 type Pool struct {
-	Timeout             *Duration `validate:"required" json:"timeout"                 yaml:"timeout"`
-	KeepAlive           *Duration `validate:"required" json:"keep-alive"              yaml:"keep-alive"`
-	IdleConnTimeout     *Duration `validate:"required" json:"idle-conn-timeout"       yaml:"idle-conn-timeout"`
-	TLSHandshakeTimeout *Duration `validate:"required" json:"tls-handshake-timeout"   yaml:"tls-handshake-timeout"`
-	MaxIdleConns        int       `validate:"required" json:"max-idle-conns"          yaml:"max-idle-conns"`
-	MaxIdleConnsPerHost int       `validate:"required" json:"max-idle-conns-per-host" yaml:"max-idle-conns-per-host"`
-	MaxConnsPerHost     int       `validate:"required" json:"max-conns-per-host"      yaml:"max-conns-per-host"`
+	Timeout             *Duration `json:"timeout"                 yaml:"timeout"                 validate:"required"`
+	KeepAlive           *Duration `json:"keep-alive"              yaml:"keep-alive"              validate:"required"`
+	IdleConnTimeout     *Duration `json:"idle-conn-timeout"       yaml:"idle-conn-timeout"       validate:"required"`
+	TLSHandshakeTimeout *Duration `json:"tls-handshake-timeout"   yaml:"tls-handshake-timeout"   validate:"required"`
+	MaxIdleConns        int       `json:"max-idle-conns"          yaml:"max-idle-conns"          validate:"required"`
+	MaxIdleConnsPerHost int       `json:"max-idle-conns-per-host" yaml:"max-idle-conns-per-host" validate:"required"`
+	MaxConnsPerHost     int       `json:"max-conns-per-host"      yaml:"max-conns-per-host"      validate:"required"`
 }
 
 type MTLS struct {
-	Enabled *bool  `validate:"required"                json:"enabled" yaml:"enabled"`
-	CA      string `validate:"required_if=Enabled true" json:"ca" yaml:"ca"`
-	Cert    string `validate:"required_if=Enabled true" json:"cert" yaml:"cert"`
-	Key     string `validate:"required_if=Enabled true" json:"key" yaml:"key"`
+	Enabled *bool  `json:"enabled" yaml:"enabled" validate:"required"`
+	CA      string `json:"ca"      yaml:"ca"      validate:"required_if=Enabled true"`
+	Cert    string `json:"cert"    yaml:"cert"    validate:"required_if=Enabled true"`
+	Key     string `json:"key"     yaml:"key"     validate:"required_if=Enabled true"`
 }
 
 type Duration struct {
